b7e837d5f8721a402848a1c6df1b6bf4f1910913
Omar Salcedo <salcedosolarteomar@gmail.com>
2026-05-18 17:51:10 -0500

    fix: configurar Prisma 7 con prisma.config.js

--- prisma.config.js
+++ prisma.config.js
@@ -1,7 +1,4 @@
-// prisma.config.js
 const { defineConfig } = require('prisma/config');
-
-// Cargamos las variables de entorno desde .env
 require('dotenv/config');
 
 module.exports = defineConfig({
